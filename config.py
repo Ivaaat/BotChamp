81062f503840798bf73bc41300cc114c75b00d6e
Ivaaat <wordnext@mail.ru>
2022-12-26 11:39:17 +0300

    удаление модулей

--- config.py
+++
@@ -1,5 +0,0 @@
-TOKEN = "5720016574:AAFuTCyQO0FQAe1R_pCTX-rYG3uBACVYXWk"
-user_id = 377190896
-User_agent = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-    }
